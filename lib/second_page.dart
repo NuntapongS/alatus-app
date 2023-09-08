f513a062235eb612210607b4275aebb4584387ef
NuntapongS <nuntapong@odds.team>
2023-09-08 14:57:05 +0700

    add second page

---
+++ lib/second_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+  @override
+  Widget build(Object context) {
+    return const Scaffold(
+      body: Center(child: Text('Second Page')),
+    );
+  }
+}
