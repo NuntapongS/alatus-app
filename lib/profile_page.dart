a4009949643d63ab247ae50838d5ba59ddb5999c
NuntapongS <nuntapong@odds.team>
2023-09-15 16:36:39 +0700

    add button in profile page

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,23 +1,40 @@
+import 'package:alatus_app/home_page.dart';
 import 'package:flutter/material.dart';
 
 class SecondPage extends StatelessWidget {
   const SecondPage({super.key});
   @override
   Widget build(Object context) {
-    return const Scaffold(
+    return Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                'Page not found',
-                style:
-                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+            child: Row(
+          children: [
+            OutlinedButton(
+              onPressed: null,
+              style: OutlinedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                side: const BorderSide(color: Colors.red, width: 3.0),
               ),
-            ],
-          )),
-        ));
+              child: const Text(
+                'Back',
+                style: TextStyle(color: Colors.red),
+              ),
+            ),
+            const SizedBox(width: 300),
+            ElevatedButton(
+                onPressed: null,
+                style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  side: const BorderSide(color: Colors.red, width: 3.0),
+                ),
+                child: const Text(
+                  'Next',
+                  style: TextStyle(color: Colors.red),
+                )),
+          ],
+        )));
   }
 }
