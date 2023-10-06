fa49d025022a7275a1de32b1bff99691ae798f73
NuntapongS <nuntapong@odds.team>
2023-10-06 11:17:32 +0700

    add button in profile page

--- lib/screen/profile_page.dart
+++ lib/screen/profile_page.dart
@@ -7,12 +7,43 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      alignment: Alignment.center,
-      decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: NetworkImage(
-                  'https://i.ytimg.com/vi/9sNQFJAb3Ss/maxresdefault.jpg'),
-              fit: BoxFit.cover)),
-    ));
+            alignment: Alignment.topRight,
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        'https://i.ytimg.com/vi/9sNQFJAb3Ss/maxresdefault.jpg'),
+                    fit: BoxFit.cover)),
+            child: SafeArea(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const HomePage()));
+                    },
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      side: const BorderSide(color: Colors.red, width: 3.0),
+                    ),
+                    child: const Text('My Profile',
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      side: const BorderSide(color: Colors.red, width: 3.0),
+                    ),
+                    child: const Text('Portfolio',
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                ],
+              ),
+            )));
   }
 }
