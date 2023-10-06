a421325cccc55d912415bc87197112e75fa47478
NuntapongS <nuntapong@odds.team>
2023-10-06 10:50:17 +0700

    revamp profile page

--- lib/screen/profile_page.dart
+++ lib/screen/profile_page.dart
@@ -6,38 +6,13 @@ class Profile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-            child: Row(
-          children: [
-            OutlinedButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const HomePage()));
-              },
-              style: OutlinedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                side: const BorderSide(color: Colors.red, width: 3.0),
-              ),
-              child: const Text(
-                'Back',
-                style: TextStyle(color: Colors.red),
-              ),
-            ),
-            const SizedBox(width: 300),
-            ElevatedButton(
-                onPressed: null,
-                style: OutlinedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  side: const BorderSide(color: Colors.red, width: 3.0),
-                ),
-                child: const Text(
-                  'Next',
-                  style: TextStyle(color: Colors.red),
-                )),
-          ],
-        )));
+        body: Container(
+      alignment: Alignment.center,
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: NetworkImage(
+                  'https://i.ytimg.com/vi/9sNQFJAb3Ss/maxresdefault.jpg'),
+              fit: BoxFit.cover)),
+    ));
   }
 }
